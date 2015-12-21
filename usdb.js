33c7f1d79c8dcd333b2a584a5bde0781c88f1b25
root <root@ip-172-31-53-234.ec2.internal>
2015-12-21 08:22:54 +0000

    good looking

--- usdb.js
+++ usdb.js
@@ -6,17 +6,17 @@ $(document).ready(function() {
 	        type: "POST",
 	        url: 'usdbstate.php',
 	        dataType: 'json',
-	        data: {functionname: 'hover', arguments: ["California"]},
+	        data: {functionname: 'hover', arguments: [stateDict[data.name]]},
 	    
 	        success: function (obj) {
 	                      if( !('error' in obj) ) {
-	                          $('#hover-div').text(obj.result.Name + " this is from the database!!");
-	 			console.log(obj.result)
+	                          $('#hover-state').text(obj.result.Name);
+				  $('#hover-pop').text("Population: " + numberWithCommas(obj.result.Population));
+	 			  console.log(obj.result)
 	                      }
 	                      else {
 	                          console.log(obj.error);
-			       	console.log("err")
-					
+			       	  console.log("err")
 	                      }
 	        }, error: function (err) {
 			console.log(err.responseText)
@@ -33,3 +33,68 @@ $(document).ready(function() {
     });
   });
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var stateDict = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District Of Columbia",
+    "FM": "Federated States Of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PW": "Palau",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+};
